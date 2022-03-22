ab706d8f547b51c2eb09ea8c00ed319493003a31
Lukas Frederiksen <luka9271@stud.kea.dk>
2022-03-22 14:54:33 +0100

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,5 +40,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/forms"),
+  ],
 };
