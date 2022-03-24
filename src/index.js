c750bc96aa8465e2d6fc26486f5a2f204608c969
Lukas Frederiksen <luka9271@stud.kea.dk>
2022-03-24 08:57:21 +0100

    madplans side indhold done - mangler style

---
+++ src/index.js
@@ -0,0 +1,13 @@
+let week = document.querySelector(".uge");
+
+week.addEventListener("click", select);
+
+function select() {
+  week.classList.add(".ugeSelected");
+
+  //   if (week.contains("ugeSelected")) {
+  //     week.classList.remove("ugeSelected");
+  //   }
+
+  console.log("juhueeeeu");
+}
